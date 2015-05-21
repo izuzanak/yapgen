22bc99cec9e88932c1ea2a57e96fae5fa7c09495
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2015-05-21 18:40:03 +0200

    added build unix/win files

---
+++ yapgen_build/build.bat
@@ -0,0 +1,7 @@
+
+:: - generate makefile -
+python mf_gen.py > Makefile
+
+:: - compile container generator -
+make -s -j 3
+
---
+++ yapgen_build/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# - generate makefile -
+python mf_gen.py > Makefile
+
+# - compile container generator -
+make -s -j 3
+
