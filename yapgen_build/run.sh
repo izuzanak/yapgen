13425c9c51de7065953a1a64940c9935718bcc40
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2015-05-21 08:15:39 +0200

    documentation parser rules
    jit parser rules
    
    json parser rules
    
    pack_code parser rules
    
    string_format parser rules
    
    rules of parsers used by uclang language

--- yapgen_build/run.sh
+++ yapgen_build/run.sh
@@ -2,5 +2,33 @@
 
 # - uclang_parser -
 #echo "uclang_parser.rules" 1>&2
-./yapgen --parser_descr rules/uclang_parser.rules --source rules/uclang_parser.src
+#./yapgen --parser_descr rules/uclang_parser.rules --source rules/uclang_parser.src
+
+# - docu_comment -
+#echo "docu_comment.rules" 1>&2
+#./yapgen --parser_descr rules/docu_comment.rules --source rules/docu_comment.src
+
+# - docu_extract -
+#echo "docu_extract.rules" 1>&2
+#./yapgen --parser_descr rules/docu_extract.rules --source rules/docu_extract.src
+
+# - jit_parser -
+#echo "jit_parser.rules" 1>&2
+#./yapgen --parser_descr rules/jit_parser.rules --source rules/jit_parser.src
+
+# - jit_types -
+#echo "jit_types.rules" 1>&2
+#./yapgen --parser_descr rules/jit_types.rules --source rules/jit_types.src
+
+# - json -
+#echo "json.rules" 1>&2
+#./yapgen --parser_descr rules/json.rules --source rules/json.src
+
+# - pack_code -
+#echo "pack_code.rules" 1>&2
+#./yapgen --parser_descr rules/pack_code.rules --source rules/pack_code.src
+
+# - string_format -
+#echo "string_format.rules" 1>&2
+./yapgen --parser_descr rules/string_format.rules --source rules/string_format.src
 
