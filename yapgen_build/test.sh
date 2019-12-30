4e2e29321246d2106816e670194f774c0ac13d48
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2019-12-30 07:00:57 +0100

    test yapgen by all parsers in rules directory

---
+++ yapgen_build/test.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+set -e
+
+# - uclang_parser -
+echo "uclang_parser.rules"
+yapgen --parser_descr rules/uclang_parser.rules --source rules/uclang_parser.src
+
+# - momas_log parser-
+echo "momas_log.rules"
+yapgen --parser_descr rules/momas_log.rules --source rules/momas_log.src
+
+# - spawner -
+echo "spawner.rules"
+yapgen --parser_descr rules/spawner.rules --source rules/spawner.src
+
+# - docu_comment -
+echo "docu_comment.rules"
+yapgen --parser_descr rules/docu_comment.rules --source rules/docu_comment.src
+
+# - docu_extract -
+echo "docu_extract.rules"
+yapgen --parser_descr rules/docu_extract.rules --source rules/docu_extract.src
+
+# - jit_parser -
+echo "jit_parser.rules"
+yapgen --parser_descr rules/jit_parser.rules --source rules/jit_parser.src
+
+# - jit_types -
+echo "jit_types.rules"
+yapgen --parser_descr rules/jit_types.rules --source rules/jit_types.src
+
+# - json -
+echo "json.rules"
+yapgen --parser_descr rules/json.rules --source rules/json.src
+
+# - json -
+echo "json_line.rules"
+yapgen --parser_descr rules/json_line.rules --source rules/json_line.src
+
+# - pack_code -
+echo "pack_code.rules"
+yapgen --parser_descr rules/pack_code.rules --source rules/pack_code.src
+
+# - string_format -
+echo "string_format.rules"
+yapgen --parser_descr rules/string_format.rules --source rules/string_format.src
+
+# - validator -
+echo "validator.rules"
+yapgen --parser_descr rules/validator.rules --source rules/validator.src
+
+# - gcg_msg_head -
+echo "gcg_msg_head.rules"
+yapgen --parser_descr rules/gcg_msg_head.rules --source rules/gcg_msg_head.src
+
+# - gcg_msg_tail -
+echo "gcg_msg_tail.rules"
+yapgen --parser_descr rules/gcg_msg_tail.rules --source rules/gcg_msg_tail.src
+
+# - uic parser -
+echo "uic.rules"
+yapgen --parser_descr rules/uic.rules --source rules/uic.src
+
+# - demo_exp -
+echo "demo_exp.rules"
+yapgen --parser_descr rules/demo_exp.rules --source rules/demo_exp.src
+
+# - demo_parse -
+echo "demo_parse.rules"
+yapgen --parser_descr rules/demo_parse/1.rules --source rules/demo_parse/1.src
+yapgen --parser_descr rules/demo_parse/2.rules --source rules/demo_parse/2.src
+yapgen --parser_descr rules/demo_parse/3.rules --source rules/demo_parse/3.src
+
+# - regexp -
+echo "regexp.rules"
+yapgen --parser_descr rules/regexp.rules --source rules/regexp.src
+
+# - packet_parser -
+echo "packet_parser.rules"
+yapgen --parser_descr rules/packet_parser.rules --source rules/packet_parser.src
+
+# - cont -
+echo "cont.rules"
+yapgen --parser_descr rules/cont.rules --source rules/cont.src
+
+# - ucl_params_options -
+echo "ucl_params_options.rules"
+yapgen --parser_descr rules/ucl_params_options.rules --source rules/ucl_params_options.src
+
+# - ksp -
+echo "ksp.rules"
+yapgen --parser_descr rules/camsys/ksp.rules --source rules/camsys/ksp.src
+
+echo "ini_cfg.rules"
+yapgen --parser_descr rules/camsys/ini_cfg.rules --source rules/camsys/ini_cfg.src
+
+# - rtsp -
+echo "rtsp.rules"
+yapgen --parser_descr rules/camsys/rtsp.rules --source rules/camsys/rtsp.src
+
+# - rtsp_sdp -
+echo "rtsp_sdp.rules"
+yapgen --parser_descr rules/camsys/rtsp_sdp.rules --source rules/camsys/rtsp_sdp.src
+
+# - fuser_fname -
+echo "fuser_fname.rules"
+yapgen --parser_descr rules/camsys/fuser_fname.rules --source rules/camsys/fuser_fname.src
+
+# - player_url -
+echo "player_url.rules"
+yapgen --parser_descr rules/camsys/player_url.rules --source rules/camsys/player_url.src
+
